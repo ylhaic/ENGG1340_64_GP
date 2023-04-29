09e98b57824392206436491041b6b30561b8e53e
肖东陆 <xiaodonglu@XDLMacBook-Pro.local>
2023-04-29 10:55:05 +0800

    update room1

--- room1.cpp
+++ room1.cpp
@@ -12,17 +12,14 @@ std::string print_and_get_room1_description() {
            "This familiar space, filled with personal belongings, offers a sense of safety and comfort. "
            "The walls are adorned with mementos of your past adventures and achievements, "
            "reminding you of the journeys you have undertaken and the wisdom you have gained.\n\n"
-           "The room's atmosphere is one of warmth and tranquility, providing a refuge from the trials "
-           "and tribulations that await you outside. The scent of a home-cooked meal lingers in the air, "
-           "and the crackling fireplace casts flickering shadows on the walls.\n\n"
            "As you prepare to embark on your quest to find the legendary spear Gungnir, "
            "you take a moment to appreciate the simple comforts of your dwelling. This is a place of "
            "solace, where you can always return to rest and recover after facing the challenges of the labyrinth.\n\n"
            "With a deep breath, you gather your courage and prepare to step out into the unknown. "
            "The fate of your quest lies in your hands.\n";
 
-    // Define the color codes for the paragraphs (cyan, green, yellow, and magenta in this case)
-    vector<string> color_codes = {"\033[36m", "\033[32m", "\033[33m", "\033[35m"};
+    // Define the color codes for the paragraphs (cyan, green, and magenta in this case)
+    vector<string> color_codes = {"\033[36m", "\033[32m", "\033[35m"};
 
     // Define the color code for emphasis (red in this case)
     string emphasis_color_code = "\033[31m";
